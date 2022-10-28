ea17513773b3401934fb7be88c35849cbd6cc218
KaiyanChu <kchu@bayviewglen.ca>
2022-10-27 22:40:18 -0400

    woop wordle didnt do the npm thing

--- Introduction to Javascript/coding-bat-map.js
+++ Introduction to Javascript/coding-bat-map.js
@@ -1,28 +1,7 @@
-//mapBully
-function mapBully(map){
-    map["b"] = map["a"];
-    map["a"] = "";
-    return map; 
-}
-console.log(mapBully({'a':'candy', 'b':'dirt'}));
-
-//topping1
-
-function topping1(map){
-    map["bread"] = "butter";
-    if(map["icecream"]){
-        map["icecream"] = "cherry";
-    }
-
-    return map; 
-}
-console.log(topping1({"pancake": "syrup"}));
-
-function mapAB2(map){
-    if(map["a"] === map ["b"]){
-        map.splice(0,2);
-    }
+//word0
+function word0(strings){
+    let map = newMap();
+    
     return map;
 }
-console.log("hi");
-console.log(mapAB2({"a": "aaa", "b": "aaa", "c": "cake"}));
+console.log
