f131fd5e7046abe4eae332c208a4ba11f457a41b
KaiyanChu <kchu@bayviewglen.ca>
2023-01-15 00:38:13 -0500

    arraystack test

--- Data Structures in Java/src/IntArrayStack.java
+++ Data Structures in Java/src/IntArrayStack.java
@@ -48,4 +48,7 @@ public class IntArrayStack {
         return arr;
     }
 
+    public int size(){
+        return arr.length;
+    }
 }
--- Data Structures in Java/src/IntArrayStackTest.java
+++ Data Structures in Java/src/IntArrayStackTest.java
@@ -1,79 +1,98 @@
+import java.nio.file.spi.FileSystemProvider;
 import java.util.Arrays;
 
 public class IntArrayStackTest {
     public static void main(String[] args) {
-        testPush();
-        testPop();
+        int testPassed = 0;
+        int testFailed = 0;
+        if(!testPush()){
+            System.out.println("Test Failed: testPush");
+            testFailed++;
+        } else {
+            testPassed++;
+        }
+        if(!testPop()){
+            System.out.println("Test Failed: testPop");
+            testFailed++;
+        } else {
+            testPassed++;
+        }
+        if(!testPeek()){
+            System.out.println("Test Failed: testPeek");
+            testFailed++;
+        } else {
+            testPassed++;
+        }
+        if(!search()){
+            System.out.println("Test Failed: search");
+            testFailed++;
+        } else{
+            testPassed++;
+        }
+        if(!empty()){
+            System.out.println("Test Failed: empty");
+            testFailed++;
+        } else{
+            testPassed++;
+        }
+        System.out.println("Tests Passed: " + testPassed + ". Tests Failed: " + testFailed);
     }
 
-    public static void testPush(){
-        //push
-        IntArrayStack arr =  new IntArrayStack();
+    private static IntArrayStack prepareArrayStack(){
+        IntArrayStack arr = new IntArrayStack();
         arr.push(1);
         arr.push(2);
         arr.push(3);
         arr.push(4);
         arr.push(5);
-        arr.push(6);
-
-        System.out.println(Arrays.toString(arr.getStack()));
-
-    }
-    public static void testPop(){
-        IntArrayStack arr =  new IntArrayStack();
-        arr.push(1);
-        arr.push(2);
-        arr.push(3);
-        arr.push(4);
-        arr.push(5);
-        arr.push(6);
-
-        arr.pop();
-        arr.pop();
-        arr.pop();
-        arr.pop();
-        arr.pop();
-        arr.pop();
-        arr.pop();
-        arr.pop();
-
-        System.out.println();
-        System.out.println(Arrays.toString(arr.getStack()));
-    }
-        //peek
-    public static void testPeek(){
-        IntArrayStack arr =  new IntArrayStack();
-        arr.push(1);
-        arr.push(2);
-        arr.push(3);
-        arr.push(4);
-        arr.push(5);
-        arr.push(6);
-        System.out.println(arr.peek());
+        return arr; 
     }
 
-    public static void search(){
-        IntArrayStack arr =  new IntArrayStack();
-        arr.push(1);
-        arr.push(2);
-        arr.push(3);
-        arr.push(4);
-        arr.push(5);
+    public static boolean testPush(){
+        IntArrayStack arr = prepareArrayStack();
+
         arr.push(6);
-        System.out.println(arr.search(2));
-        System.out.println(arr.search(1));
+        if(arr.getStack()[5] != 6 || arr.size() != 6){
+            return false;
+        }
+
+        return true;
+    }
+    public static boolean testPop(){
+        IntArrayStack arr =  prepareArrayStack();
+
+        arr.pop();
+        
+        if(arr.size() != 4 || arr.getStack()[0] != 1)
+            return false;
+        
+        return true;
     }
 
-    public static void empty(){
-        IntArrayStack arr =  new IntArrayStack();
-        arr.push(1);
-        arr.push(2);
-        arr.push(3);
-        arr.push(4);
-        arr.push(5);
-        arr.push(6);
-        System.out.println(arr.empty());
+    public static boolean testPeek(){
+        IntArrayStack arr =  prepareArrayStack();
+        if(arr.peek() != 5)
+            return false;
+        return true;
+    }
 
+    public static boolean search(){
+        IntArrayStack arr =  prepareArrayStack();
+        if(arr.search(2) != 3 || arr.search(6) != -1)
+            return false;
+        return true;
+    }
+
+    public static boolean empty(){
+        IntArrayStack arr =  prepareArrayStack();
+        if(arr.empty())
+            return false;
+        for (int i = 0; i < 5; i++) {
+            arr.pop();
+        }
+        if(!arr.empty())
+            return false;
+        return true;
     }
 
     }
